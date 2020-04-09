a8d3bdf5ea46342a530a244153f4d7251cdcdffe
sar <safonov.a.r@mail.ru>
2020-04-09 23:51:35 +0300

    Integrate migrations

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # donkey-engine
+
+## Migrations
+
+Make migration:
+```
+src/migrations/makemigrations.sh migration_message
+```
+
+Apply migration:
+```
+src/migrations/migrate.sh
+```
--- src/db.py
+++ src/db.py
@@ -7,14 +7,18 @@ from src import settings
 db = Gino()
 
 
+def gen_postgres_url():
+    """Make string for connection to database."""
+    return 'postgresql://{0}:{1}@{2}:{3}/{4}'.format(
+        settings.DATABASE_USER,
+        settings.DATABASE_PASS,
+        settings.DATABASE_HOST,
+        settings.DATABASE_PORT,
+        settings.DATABASE_NAME,
+    )
+
+
 async def init_db():
     """Init database connection."""
-    await db.set_bind(
-        'postgresql://{0}:{1}@{2}:{3}/{4}'.format(
-            settings.DATABASE_USER,
-            settings.DATABASE_PASS,
-            settings.DATABASE_HOST,
-            settings.DATABASE_PORT,
-            settings.DATABASE_NAME,
-        )
-    )
+    url = gen_postgres_url()
+    await db.set_bind(url)
